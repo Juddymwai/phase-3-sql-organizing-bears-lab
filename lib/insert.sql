bf2972d609127937bc0f24e142da369290942b94
Judy Mwai <juddiemukami@gmail.com>
2022-08-22 12:53:29 +0300

    done

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO bears(name, age, sex, color, temperament, alive)
+VALUES ("Mr. Choclate", 10,"M","Brown", "calm", true);
+
+INSERT INTO bears(name, age, sex, color, temperament, alive)
+VALUES ("Rowdy", 5,"M","Brown", "joyous", true);
+
+INSERT INTO bears(name, age, sex, color, temperament, alive)
+VALUES ("Tabitha", 10,"F","Brown", "calm", true);
+
+INSERT INTO bears(name, age, sex, color, temperament, alive)
+VALUES ("Sergeant Brown", 15,"M","Dark", "Angry", false);
+
+INSERT INTO bears(name, age, sex, color, temperament, alive)
+VALUES ("Mellisa", 10,"F","Brown", "calm", true);
+
+INSERT INTO bears(name, age, sex, color, temperament, alive)
+VALUES ("Grinch", 10,"M","Brown", "calm", true);
+
+INSERT INTO bears(name, age, sex, color, temperament, alive)
+VALUES ("Wendy", 10,"F","Brown", "stubborn", true);
+
+INSERT INTO bears(name, age, sex, color, temperament, alive)
+VALUES (Null, 10,"M","Brown", "calm", true);
